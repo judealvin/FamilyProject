39f096250096309b6c7d7e80520b0622f99788a1
jude <judeluyirikaa@gmail.com>
2026-06-29 08:40:19 +0300

    Family tracker project structure
    Blueprint

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/client/pages/**/*.html",
+    "./src/client/scripts/**/*.ts",
+    "./src/client/components/**/*.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
